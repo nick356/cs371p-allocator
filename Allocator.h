d91d2398c4c23b569634675890f9c8d3f983410e
Nicholas Carter <nick356@cs.utexas.edu>
2014-10-22 17:06:05 -0500

    Added some unit tests, and I think I have finished the Allocator method. Working on allocate

--- Allocator.h
+++ Allocator.h
@@ -20,150 +20,194 @@
 // Allocator
 // ---------
 
+using namespace std;
+
 template <typename T, int N>
 class Allocator {
-    public:
-        // --------
-        // typedefs
-        // --------
+	public:
+		// --------
+		// typedefs
+		// --------
 
-        typedef T                 value_type;
+		typedef T                 value_type;
 
-        typedef std::size_t       size_type;
-        typedef std::ptrdiff_t    difference_type;
+		typedef std::size_t       size_type;
+		typedef std::ptrdiff_t    difference_type;
 
-        typedef       value_type*       pointer;
-        typedef const value_type* const_pointer;
+		typedef       value_type*       pointer;
+		typedef const value_type* const_pointer;
 
-        typedef       value_type&       reference;
-        typedef const value_type& const_reference;
+		typedef       value_type&       reference;
+		typedef const value_type& const_reference;
 
-    public:
-        // -----------
-        // operator ==
-        // -----------
+	public:
+		// -----------
+		// operator ==
+		// -----------
 
-        friend bool operator == (const Allocator&, const Allocator&) {
-            return true;}                                              // this is correct
+		friend bool operator == (const Allocator&, const Allocator&) {
+			return true;
+		}                                              // this is correct
 
-        // -----------
-        // operator !=
-        // -----------
+		// -----------
+		// operator !=
+		// -----------
 
-        friend bool operator != (const Allocator& lhs, const Allocator& rhs) {
-            return !(lhs == rhs);}
+		friend bool operator != (const Allocator& lhs, const Allocator& rhs) {
+			return !(lhs == rhs);
+		}
 
-    private:
-        // ----
-        // data
-        // ----
+	private:
+		// ----
+		// data
+		// ----
 
-        char a[N];
+		char a[N];
 
-        // -----
-        // valid
-        // -----
+		// -----
+		// valid
+		// -----
 
-        /**
-         * O(1) in space
-         * O(n) in time
-         * <your documentation>
-         */
-        bool valid () const {
-            // <your code>
-            return true;}
+		/**
+		* O(1) in space
+		* O(n) in time
+		* <your documentation>
+		*/
+		bool valid () const {
+			int i = 0;
+			bool isVal = true;
+			while(i<N-sizeof(T)){
+				if(a[i] != a[a[i]+sizeof(T)])
+					isVal = false;
+				cout<<"i is: "<< i<<" and a[i] is: "<<(int)a[i]<<endl;
+				cout<<"a[i]+sizeof(T) is: "<<(int)a[i]+sizeof(T)<<" and a[a[i]+sizeof(T)] is: "<< (int)a[a[i]+sizeof(T)]<<endl;
+				i = a[i]+sizeof(T);
+			}
+			return isVal;
+		}
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        int& view (int i) {
-            return *reinterpret_cast<int*>(&a[i]);}
+		/**
+		* O(1) in space
+		* O(1) in time
+		* <your documentation>
+		*/
+		int& view (int i) {
+			return *reinterpret_cast<int*>(&a[i]);
+		}
 
-    public:
-        // ------------
-        // constructors
-        // ------------
+	public:
+		// ------------
+		// constructors
+		// ------------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * throw a bad_alloc exception, if N is less than sizeof(T) + (2 * sizeof(int))
-         */
-        Allocator () {
-            // <your code>
-            assert(valid());}
+		/**
+		* O(1) in space
+		* O(1) in time
+		* throw a bad_alloc exception, if N is less than sizeof(T) + (2 * sizeof(int))
+		*/
+		Allocator () {
+			if(N < sizeof(T) + (2 * sizeof(T)))
+				throw;
+			a[0] = N - 2*sizeof(T);
+			a[N-sizeof(T)] = N - 2*sizeof(T);
 
-        // Default copy, destructor, and copy assignment
-        // Allocator  (const Allocator&);
-        // ~Allocator ();
-        // Allocator& operator = (const Allocator&);
+			assert(valid());
+		}
 
-        // --------
-        // allocate
-        // --------
+		// Default copy, destructor, and copy assignment
+		// Allocator  (const Allocator&);
+		// ~Allocator ();
+		// Allocator& operator = (const Allocator&);
 
-        /**
-         * O(1) in space
-         * O(n) in time
-         * after allocation there must be enough space left for a valid block
-         * the smallest allowable block is sizeof(T) + (2 * sizeof(int))
-         * choose the first block that fits
-         * return 0, if allocation fails
-         */
-        pointer allocate (size_type n) {
-            // <your code>
-            assert(valid());
-            return 0;}                   // replace!
+		// --------
+		// allocate
+		// --------
 
-        // ---------
-        // construct
-        // ---------
+		/**
+		* O(1) in space
+		* O(n) in time
+		* after allocation there must be enough space left for a valid block
+		* the smallest allowable block is sizeof(T) + (2 * sizeof(int))
+		* choose the first block that fits
+		* return 0, if allocation fails
+		*/
+		pointer allocate (size_type n) {
+			assert(valid());
+			bool isFound = false;
+			int sizeofN = sizeof(T) * n;
+			int i = 0;
+			while(i<N-sizeof(T) && !isFound){
+				if(a[i]<0){
+					int temp = -1*a[i];
+					i = i+temp + sizeof(T);
+				}else if(a[i] < sizeofN){
+					i = i + a[i] +sizeof(T) ;
+				}else{
+					int orig_size = a[i];
+					int orig_last = i+a[i]+sizeof(T);
+					
+					a[i] = -1 * sizeofN;
+					a[i + sizeofN + sizeof(T)] = -1 * sizeofN;
+					i = i + sizeofN +(2*sizeof(T));
+					a[
+				}
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        void construct (pointer p, const_reference v) {
-            new (p) T(v);                               // this is correct and exempt
-            assert(valid());}                           // from the prohibition of new
 
-        // ----------
-        // deallocate
-        // ----------
+			}
+			return 0;
+		}// replace!
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * after deallocation adjacent free blocks must be coalesced
-         * <your documentation>
-         */
-        void deallocate (pointer p, size_type) {
-            // <your code>
-            assert(valid());}
+		// ---------
+		// construct
+		// ---------
 
-        // -------
-        // destroy
-        // -------
+		/**
+		* O(1) in space
+		* O(1) in time
+		* <your documentation>
+		*/
+		void construct (pointer p, const_reference v) {
+			new (p) T(v);                               // this is correct and exempt
+			assert(valid());
+		} 			                          // from the prohibition of new
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * throw an invalid_argument exception, if pointer is invalid
-         * <your documentation>
-         */
-        void destroy (pointer p) {
-            p->~T();               // this is correct
-            assert(valid());}
+		// ----------
+		// deallocate
+		// ----------
 
-        /**
-         * O(1) in space
-         * O(1) in time
-         * <your documentation>
-         */
-        const int& view (int i) const {
-            return *reinterpret_cast<const int*>(&a[i]);}};
+		/**
+		* O(1) in space
+		* O(1) in time
+		* after deallocation adjacent free blocks must be coalesced
+		* <your documentation>
+		*/
+		void deallocate (pointer p, size_type) {
+			// <your code>
+			assert(valid());
+		}
 
+		// -------
+		// destroy
+		// -------
+
+		/**
+		* O(1) in space
+		* O(1) in time
+		* throw an invalid_argument exception, if pointer is invalid
+		* <your documentation>
+		*/
+		void destroy (pointer p) {
+			p->~T();               // this is correct
+			assert(valid());
+		}
+
+		/**
+		* O(1) in space
+		* O(1) in time
+		* <your documentation>
+		*/
+		const int& view (int i) const {
+			return *reinterpret_cast<const int*>(&a[i]);
+		}
+};
 #endif // Allocator_h
--- TestAllocator.c++
+++ TestAllocator.c++
@@ -21,14 +21,23 @@
 
 template <typename A>
 struct TestAllocator : testing::Test {
-    // --------
-    // typedefs
-    // --------
+	// --------
+	// typedefs
+	// --------
 
-    typedef          A                  allocator_type;
-    typedef typename A::value_type      value_type;
-    typedef typename A::difference_type difference_type;
-    typedef typename A::pointer         pointer;};
+	typedef          A                  allocator_type;
+	typedef typename A::value_type      value_type;
+	typedef typename A::difference_type difference_type;
+	typedef typename A::pointer         pointer;
+};
+
+template <typename A>
+struct TestView : testing::Test{
+	typedef          A                  allocator_type;
+	typedef typename A::value_type      value_type;
+	typedef typename A::difference_type difference_type;
+	typedef typename A::pointer         pointer;
+};
 
 typedef testing::Types<
             std::allocator<int>,
@@ -37,50 +46,235 @@ typedef testing::Types<
             Allocator<double, 100> >
         my_types;
 
+typedef testing::Types<
+		Allocator<int,100>,
+		Allocator<double, 100> >
+	class_types;	
+
 TYPED_TEST_CASE(TestAllocator, my_types);
+TYPED_TEST_CASE(TestView, class_types);
+
 
 TYPED_TEST(TestAllocator, One) {
-    typedef typename TestFixture::allocator_type  allocator_type;
-    typedef typename TestFixture::value_type      value_type;
-    typedef typename TestFixture::difference_type difference_type;
-    typedef typename TestFixture::pointer         pointer;
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
 
-    allocator_type x;
-    const difference_type s = 1;
-    const value_type      v = 2;
-    const pointer         p = x.allocate(s);
-    if (p != 0) {
-        x.construct(p, v);
-        ASSERT_EQ(v, *p);
-        x.destroy(p);
-        x.deallocate(p, s);}}
+	allocator_type x;
+	const difference_type s = 1;
+	const value_type      v = 2;
+	const pointer         p = x.allocate(s);
+	if (p != 0) {
+		x.construct(p, v);
+		ASSERT_EQ(v, *p);
+		x.destroy(p);
+		x.deallocate(p, s);
+	}
+}
+
 
 TYPED_TEST(TestAllocator, Ten) {
-    typedef typename TestFixture::allocator_type  allocator_type;
-    typedef typename TestFixture::value_type      value_type;
-    typedef typename TestFixture::difference_type difference_type;
-    typedef typename TestFixture::pointer         pointer;
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
 
-    allocator_type x;
-    const difference_type s = 10;
-    const value_type      v = 2;
-    const pointer         b = x.allocate(s);
-    if (b != 0) {
-        pointer e = b + s;
-        pointer p = b;
-        try {
-            while (p != e) {
-                x.construct(p, v);
-                ++p;}}
-        catch (...) {
-            while (b != p) {
-                --p;
-                x.destroy(p);}
-            x.deallocate(b, s);
-            throw;}
-        ASSERT_EQ(s, std::count(b, e, v));
-        while (b != e) {
-            --e;
-            x.destroy(e);}
-        x.deallocate(b, s);}}
+	allocator_type x;
+	const difference_type s = 10;
+	const value_type      v = 2;
+	const pointer         b = x.allocate(s);
+	if (b != 0) {
+		pointer e = b + s;
+		pointer p = b;
+		try {
+			while (p != e) {
+				x.construct(p, v);
+				++p;
+			}
+		}
+		catch (...) {
+			while (b != p) {
+				--p;
+				x.destroy(p);
+			}
+			x.deallocate(b, s);
+		throw;
+		}
+		ASSERT_EQ(s, std::count(b, e, v));
+		while (b != e) {
+			--e;
+			x.destroy(e);
+		}
+		x.deallocate(b, s);
+	}
+}
+
+TYPED_TEST(TestAllocator, Eleven){
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
+
+	allocator_type x;
+	const difference_type	s = 5;
+	const value_type	v = 4;
+	const pointer		b = x.allocate(s);
+	if(b != 0){
+		pointer end = b + s;
+		pointer point = b;
+		try{
+			while(point != end){
+				x.construct(point, v);
+				++point;
+			}
+		}catch(...){
+			while(b != point){
+				--point;
+				x.destroy(point);
+			}
+			x.deallocate(b, s);
+		throw;
+		}
+		ASSERT_EQ(s, std::count(b,end,v));
+		while(b != end){
+			--end;
+			x.destroy(end);
+		}
+		x.deallocate(b, s);
+	}
+}	
+
+TYPED_TEST(TestAllocator, Twelve){
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
+
+	allocator_type x;
+	const difference_type	s = 100;
+	const value_type	v = 4;
+	const pointer		b = x.allocate(s);
+	if(b != 0){
+		pointer end = b + s;
+		pointer point = b;
+		try{
+			while(point != end){
+				x.construct(point, v);
+				++point;
+			}
+		}catch(...){
+			while(b != point){
+				--point;
+				x.destroy(point);
+			}
+			x.deallocate(b, s);
+		throw;
+		}
+		ASSERT_EQ(s, std::count(b,end,v));
+		while(b != end){
+			--end;
+			x.destroy(end);
+		}
+		x.deallocate(b, s);
+	}
+}
+
+TYPED_TEST(TestView, TestView1){
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
+
+	allocator_type x;
+	const difference_type s = 1;
+	const value_type      v = 2;
+	const pointer         p = x.allocate(s);
+	if (p != 0) {
+		x.construct(p, v);
+		const allocator_type& temp = x;
+		const int& sigh = temp.view(0);
+		ASSERT_EQ(sigh, 2);
+		x.destroy(p);
+		x.deallocate(p, s);
+	}
+}
+
+TYPED_TEST(TestView, TestView2){
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
+
+	allocator_type x;
+	const difference_type	s = 100;
+	const value_type	v = 4;
+	const pointer		b = x.allocate(s);
+	if(b != 0){
+		pointer end = b + s;
+		pointer point = b;
+		try{
+			while(point != end){
+				x.construct(point, v);
+				++point;
+			}
+		}catch(...){
+			while(b != point){
+				--point;
+				x.destroy(point);
+			}
+			x.deallocate(b, s);
+		throw;
+		}
+		const allocator_type& temp = x;
+		const int& sigh = temp.view(0);
+		ASSERT_EQ(sigh, 4);
+		while(b != end){
+			--end;
+			x.destroy(end);
+		}
+		x.deallocate(b, s);
+	}
+}
+
+TYPED_TEST(TestView, TestView3){
+	typedef typename TestFixture::allocator_type  allocator_type;
+	typedef typename TestFixture::value_type      value_type;
+	typedef typename TestFixture::difference_type difference_type;
+	typedef typename TestFixture::pointer         pointer;
+
+	allocator_type x;
+	const difference_type	s = 100;
+	const value_type	v = 4;
+	const pointer		b = x.allocate(s);
+	if(b != 0){
+		pointer end = b + s;
+		pointer point = b;
+		try{
+			while(point != end){
+				x.construct(point, v);
+				++point;
+			}
+		}catch(...){
+			while(b != point){
+				--point;
+				x.destroy(point);
+			}
+			x.deallocate(b, s);
+		throw;
+		}
+		const allocator_type& temp = x;
+		const int& sigh = temp.view(0);
+		const int& sigh2 = temp.view(1);
+		ASSERT_EQ(sigh, 4);
+		ASSERT_EQ(sigh2, 4);
+		while(b != end){
+			--end;
+			x.destroy(end);
+		}
+		x.deallocate(b, s);
+	}
+}
+
+
 
